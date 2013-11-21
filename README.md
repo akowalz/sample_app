b90b233e01241f3178fe069864517ea811ce0b68
Alex Kowalczuk <askowalczuk93@gmail.com>
2013-11-21 00:08:37 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Rails Tutorial Sample App
+
+This the sample app for the rails tutorial book by Michael Hartl.
+
+Putting on Github mostly for backup purposes, in case I totally screw something up.
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SampleApp::Application.config.secret_key_base = '7f19da43528f025ed8e814691a91b8464a38ccde7a01d3abbb679d688a2c54a6b44ff68b04875d1e17faaab5647450095db452458dd2bdc0b85572d8d902a4dd'
